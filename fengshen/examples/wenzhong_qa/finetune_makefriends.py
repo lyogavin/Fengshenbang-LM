ee565069946369bb22fd5fee366f991a07ef8c66
Yu Li <lyo.gavin@gmail.com>
2022-11-19 10:52:28 -0600

    10 epochs and output validation result each epoch

--- fengshen/examples/wenzhong_qa/finetune_makefriends.py
+++ fengshen/examples/wenzhong_qa/finetune_makefriends.py
@@ -149,7 +149,7 @@ class GPT2FinetuneMedicalQA(pl.LightningModule):
         # acc = self.comput_metrix(output.logits, batch['labels'])
         self.log('val_loss', output.loss)
         # self.log('val_acc', acc)
-        prediction = generate_agent_paraphrase(self.model, self.tokenizer, batch['input_ids'])
+        prediction = generate_agent_paraphrase(self.model, self.tokenizer, batch['input_ids'].unsqueeze(0))
         self.log('validation_samples', f"labels: {batch['answer']}\npredictions: {prediction}")
 
     def configure_optimizers(self):
