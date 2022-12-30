68d472ffd377514d4fef3267c267018ce2f14ea9
Yu Li <lyo.gavin@gmail.com>
2022-12-29 22:51:09 -0600

    bloomz combined

--- fengshen/examples/wenzhong_qa/finetune_bloomz_combine_highinter_template_suspence.sh
+++ fengshen/examples/wenzhong_qa/finetune_bloomz_combine_highinter_template_suspence.sh
@@ -118,7 +118,7 @@ DATA_ARGS="
     --train_data  $train_data_file \
     --valid_data  $val_data_file \
     --test_data  $val_data_file \
-    --max_seq_length 550 \
+    --max_seq_length 450 \
 "
 
 # PRETRAINED_MODEL_PATH=/cognitive_comp/wuziwei/pretrained_model_hf/gpt2
