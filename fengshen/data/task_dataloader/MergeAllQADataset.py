5217f6b53ecd41569bd6618d25f66ef73443f566
Yu Li <lyo.gavin@gmail.com>
2023-01-09 09:24:35 -0600

    truncate title in case title too long

--- fengshen/data/task_dataloader/MergeAllQADataset.py
+++ fengshen/data/task_dataloader/MergeAllQADataset.py
@@ -100,7 +100,7 @@ class GPT2QADataset(Dataset):
 
         max_length_left = self.max_seq_length - len(postfix_input_ids)
         max_length_left = max(3,max_length_left)
-        print(f"max_length_left:{max_length_left}")
+        #print(f"max_length_left:{max_length_left}")
         prefix_input_ids = self.tokenizer.encode(prompted_content, truncation=True, max_length=max_length_left)
 
 
@@ -205,7 +205,7 @@ if __name__ == '__main__':
     for title_len in range(80, 150):
         title = ['数'] * title_len
         title = ''.join(title)
-        testml.max_seq_length = 100
+        testml.max_seq_length = 200
         print(f"title len:{title_len}")
 
         res = testml.encode({"data_type":"redbook_content_title",
